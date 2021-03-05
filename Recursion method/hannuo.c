1fcd025e748c17fe81d9ba83b76c9599cad4a55e
爱学习的小鹏友 <50280531+anbooks@users.noreply.github.com>
2021-03-05 16:58:02 +0800

    Create hannuo.c
    经典汉诺塔问题

---
+++ Recursion method/hannuo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+move(int n,int x,int y,int z)
+{
+    if(n==1)
+    printf("%c-->%c\n",x,z);
+    else
+    {
+        move(n-1,x,z,y);
+        printf("%c-->%c\n",x,z);
+       // {   
+       // getchar();
+       // }
+        move(n-1,y,x,z);
+
+    }
+}
+int main()
+{
+    int h;
+    printf("输入盘子个数：");
+    scanf("%d",&h);
+    printf("移动%2d个盘子的步骤如下：\n",h);
+    move(h,'a','b','c');
+    system("pause");
+
+}
