a3bb9fdc65b5cb9a01a6c9f0c9e23cd9ded238f8
爱学习的小鹏友 <50280531+anbooks@users.noreply.github.com>
2021-11-29 21:50:10 +0800

    Create 三个数排序.c
    从键盘输入三个整数a、b、c，将其按大到小排列。

---
+++ 0基础C语言代码/三个数排序.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+ int main()
+ {
+   int a,b,c,temp;
+   scanf("%d,%d,%d",&a,&b,&c);
+   if(a<b)
+   {
+     temp=a;
+     a=b;
+     b=temp ;
+    }   //a小就换，把大的换到前面
+ 
+   if(a<c)     
+   {
+     temp=a;
+     a=c;
+     c=temp ;
+   }      
+
+   if(b<c)
+   {
+     temp=b;
+     b=c;
+     c=temp ;
+    }
+   printf("%d,%d,%d\n", a,b,c);
+  
+   return 0;
+ }
