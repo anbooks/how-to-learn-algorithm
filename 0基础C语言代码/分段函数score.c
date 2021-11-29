ceef6f2efd818c233cdc130deed390dcf078d317
爱学习的小鹏友 <50280531+anbooks@users.noreply.github.com>
2021-11-29 20:50:59 +0800

    Create 分段函数score.c
    输入一个整数，当为65时显示A，66时显示B，68时显示C，其他值时显示END

---
+++ 0基础C语言代码/分段函数score.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+int x;
+scanf("%d",&x);
+  if(x==65)
+  {
+  printf("x=%d,show %c\n",x,x);
+  }
+  else if(x==66)
+  {
+    printf(“x=%d, show %c\n”,x,x);
+  }
+  else if(x==67)
+  {
+    printf(“x=%d, show %c\n”,x,x);
+  }
+  else 
+  {
+    printf(“x=%d, show END\n”,x);
+  }
+return 0;
+}
