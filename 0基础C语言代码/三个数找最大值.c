01c0f1a53cd7cbaf1c35389fddbc8a3cff206e67
爱学习的小鹏友 <50280531+anbooks@users.noreply.github.com>
2021-11-30 04:59:37 +0800

    Create 三个数找最大值.c
    从键盘输入三个整数a、b、c，输出其中最大的数

---
+++ 0基础C语言代码/三个数找最大值.c
@@ -0,0 +1,17 @@
+#include “stdio.h”
+int main()
+{
+  int a,b,c,max;
+  scanf("%d %d %d",&a,&b,&c);
+  if(a>b)
+  {
+    if(a>c)max =a;
+  }
+  else
+  {
+    if(b>c) max =b;
+    else max =c;
+   }
+  printf("%d\n", max);
+return 0;
+}
